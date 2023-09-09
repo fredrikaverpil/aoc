bbff15949485aaa70eb21b220464a44bff58426e
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-09-09 15:04:36 +0200

    feat: add day1

---
+++ dec1.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func readFile(filename string) []byte {
+	contents, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return contents
+}
+
+func fileContentsToMap(contents []byte) map[int][]int {
+	lines := strings.Split(string(contents), "\n")
+	contentMap := make(map[int][]int)
+
+	key := 0
+	for _, line := range lines {
+		if line == "" {
+			key++
+		} else {
+			lineAsInt, err := strconv.Atoi(line)
+			if err != nil {
+				log.Fatal(err)
+			}
+			contentMap[key] = append(contentMap[key], lineAsInt)
+		}
+	}
+
+	return contentMap
+}
+
+func contentMapSums(contentMap map[int][]int) []int {
+	sums := []int{}
+
+	for _, v := range contentMap {
+		sum_weights := 0
+		for _, weight := range v {
+			sum_weights += weight
+		}
+		sums = append(sums, sum_weights)
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(sums)))
+
+	return sums
+}
+
+func part1(puzzleInput []byte) {
+	contentMap := fileContentsToMap(puzzleInput)
+	sums := contentMapSums(contentMap)
+	log.Printf("Largest weight: %d", sums[0])
+}
+
+func part2(puzzleInput []byte) {
+	contentMap := fileContentsToMap(puzzleInput)
+	sums := contentMapSums(contentMap)
+	topThree := sums[0] + sums[1] + sums[2]
+
+	log.Printf("Top three largest weights combined: %d", topThree)
+}
+
+func main() {
+	puzzleInput := readFile("dec1.txt")
+	part1(puzzleInput)
+	part2(puzzleInput)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fredrikaverpil/aoc
+
+go 1.21.0
